fbf47e07a4d1037b01ed3acea66123625c793238
Vilguax <axel.pelassa@hotmail.fr>
2023-03-01 19:24:11 +0100

    les bulles

--- ex07/ft_print_bubble_sort.js
+++ ex07/ft_print_bubble_sort.js
@@ -7,6 +7,24 @@ fonction doit modifier le tableau d'origine et afficher le tableau trié.
 */
 
 function ft_print_bubble_sort() {
-
+    var tab = [2,3,5,1,4,6,7,8,9,10];
+    var i = 0;
+    var j = 0;
+    var temp = 0;
+    var len = tab.length;
+    while (i < len) {
+        while (j < len) {
+            if (tab[i] < tab[j]) {
+                temp = tab[i];
+                tab[i] = tab[j];
+                tab[j] = temp;
+            }
+            j++;
+        }
+        i++;
+        j = 0;
+    }
+    console.log(tab);
 }
+
 ft_print_bubble_sort();
