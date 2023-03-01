5bd50678884ab9d8a9b41d359c946498be404250
Vilguax <axel.pelassa@hotmail.fr>
2023-03-01 18:13:28 +0100

    fonction 1/2 du tableau en para

--- ex02/ft_print_half_array.js
+++ ex02/ft_print_half_array.js
@@ -5,7 +5,14 @@ case sur deux d’un tableau de reçu en paramètre.
 
 */
 
-function ft_print_half_array() {
+function ft_print_half_array(array) {
+    var i = 0;
+    while (i < array.length) {
+        if (i % 2 === 0) {
+            console.log(array[i]);
+        }
+        i++;
+    }
 
 }
-ft_print_half_array();
+ft_print_half_array([1,2,3,4]);
