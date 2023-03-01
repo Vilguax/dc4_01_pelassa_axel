bd09d329cbebc1eb5def35bb8bff4a5c092b5203
Vilguax <axel.pelassa@hotmail.fr>
2023-03-01 18:56:45 +0100

    oui

--- ex06/ft_print_insertion_sort.js
+++ ex06/ft_print_insertion_sort.js
@@ -6,7 +6,23 @@ d'origine et afficher le tableau trié.
 
 */
 
-function ft_print_insertion_sort() {
+function ft_print_insertion_sort(tab) {
+    let i = 1;
+    let j = 0;
+    let temp = 0;
+    
+    while (i < tab.length) {
+        j = i;
+        while (j > 0 && tab[j] < tab[j - 1]) {
+        temp = tab[j];
+        tab[j] = tab[j - 1];
+        tab[j - 1] = temp;
+        j--;
+        }
+        i++;
+    }
+    
+    console.log(tab);
+    }
 
-}
-ft_print_insertion_sort();
+ft_print_insertion_sort([5,12,16,3,8,9,10,1,2,4,6,7,11,13,14,15,17,18,19,20]);
