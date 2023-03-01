63cab6de87c6692bae90a5aa7f8eba80b393a279
Vilguax <axel.pelassa@hotmail.fr>
2023-03-01 19:48:08 +0100

    aze

--- ex07/ft_print_bubble_sort.js
+++ ex07/ft_print_bubble_sort.js
@@ -10,14 +10,14 @@ function ft_print_bubble_sort() {
     var tab = [2,3,5,1,4,6,7,8,9,10];
     var i = 0;
     var j = 0;
-    var temp = 0;
+    var tampon = 0;
     var len = tab.length;
     while (i < len) {
         while (j < len) {
             if (tab[i] < tab[j]) {
-                temp = tab[i];
+                tampon = tab[i];
                 tab[i] = tab[j];
-                tab[j] = temp;
+                tab[j] = tampon;
             }
             j++;
         }
